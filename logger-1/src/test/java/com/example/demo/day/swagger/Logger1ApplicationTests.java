17e88ea61c6d75261f95246b10fed8f9cedf5133
Swathi <108917056+swathhi@users.noreply.github.com>
2023-09-01 10:05:06 +0530

    Add files via upload

---
+++ logger-1/src/test/java/com/example/demo/day/swagger/Logger1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.day.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Logger1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
